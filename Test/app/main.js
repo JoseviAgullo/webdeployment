4dfba16388b9137af848dfa75ba81bc87f48fd71
Quique Fdez Guerra <quique@ckgrafico.com>
2014-11-19 16:14:41 +0100

    add main

---
+++ Test/app/main.js
@@ -0,0 +1,27 @@
+﻿requirejs.config({
+    paths: {
+        'text': '../lib/require/text',
+        'durandal':'../lib/durandal/js',
+        'plugins' : '../lib/durandal/js/plugins',
+        'transitions' : '../lib/durandal/js/transitions',
+        'knockout': '../lib/knockout/knockout-3.1.0',
+        'jquery': '../lib/jquery/jquery-1.9.1'
+    }
+});
+
+define(['durandal/system', 'durandal/app', 'durandal/viewLocator'],  function (system, app, viewLocator) {
+    app.title = 'Universidad Rey Juan Carlos';
+
+    app.configurePlugins({
+        router:true,
+        dialog: true,
+        widget: {
+            kinds: ['expander']
+        }
+    });
+
+    app.start().then(function () {
+        viewLocator.useConvention();
+        app.setRoot('shell');
+    });
+});
